151bce647b44bb230a6eb40fa06eee144b9c50ea
New Future <github@newfuture.cc>
2018-09-28 00:49:00 +0800

    update fetch

--- fetch.ts
+++ fetch.ts
@@ -1,4 +1,42 @@
 /**
+ * fetch 封装
+ * @see https://developer.mozilla.org/zh-CN/docs/Web/API/Fetch_API
+ * @param url 请求URL或者Request对象
+ * @param init 请求参数
+ */
+export function fetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
+
+    init = init || {}
+
+    const param: any = ((typeof url) !== 'object') ?
+        {
+            url,
+            data: init.body,
+            header: init.headers || {},
+            method: init.method,
+            dataType: 'text'
+        } : {
+            url: (url as Request).url,
+            data: (url as Request).json() || (url as Request).text() || (url as Request).body,
+            method: init.method || (url as Request).method,
+            header: Object.assign({}, (url as Request).headers, init.headers),
+            dataType: 'text',
+
+        };
+
+    return new Promise<Response>((resolve, reject) => {
+        param.success = function (res: any) {
+            var response = new wxHttpResponse(res);
+            resolve(response);
+        };
+        param.fail = reject;
+        param.complete = init['complete'];
+        RequestMQ.request(param);
+    })
+}
+
+
+/**
  * Request 请求队列管理
  */
 const RequestMQ = {
@@ -52,18 +90,21 @@ class wxHttpResponse implements Response {
     trailer: Promise<Headers>;
     type: ResponseType;
     url: string;
-    data: any;
-    constructor(res: {data:any,statusCode:number,header:Headers}) {
+    body: ReadableStream | any;
+    bodyUsed: boolean;
+    constructor(res: { data: any, statusCode: number, header: Headers }) {
         this.status = res.statusCode;
         this.headers = res.header;
-        this.data = res.data;
+        this.body = res.data;
         this.ok = res.statusCode >= 200 && res.statusCode < 300;
+        this.bodyUsed = false
     }
+    /**
+     * Copy
+     */
     clone(): Response {
-        throw new Error("Method not implemented.");
+        return Object.assign(this);
     }
-    body: ReadableStream;
-    bodyUsed: boolean;
     arrayBuffer(): Promise<ArrayBuffer> {
         throw new Error("Method not implemented.");
     }
@@ -73,45 +114,30 @@ class wxHttpResponse implements Response {
     formData(): Promise<FormData> {
         throw new Error("Method not implemented.");
     }
+    /**
+     * to json as promise
+     */
     json(): Promise<any> {
         return new Promise((resolve, reject) => {
             try {
-                resolve(JSON.stringify(this.data));
+                resolve(JSON.parse(this.body));
+                this.bodyUsed = true;
             } catch (error) {
                 reject(error);
             }
         })
     }
+    /**
+     * to string as promise
+     */
     text(): Promise<string> {
         return new Promise((resolve, reject) => {
             try {
-                resolve(this.data);
+                resolve(this.body);
+                this.bodyUsed = true;
             } catch (error) {
                 reject(error);
             }
         })
     }
-}
-
-
-/**
- * fetch 封装
- */
-export function fetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
-  return new Promise<Response>((resolve, reject) => {
-    var param = {
-          url,
-          data: init.body,
-          header: init.headers || {},
-          method: init.method,
-          dataType: 'text',
-          success: function (res: any) {
-            var response = new wxHttpResponse(res);
-            resolve(response);
-          },
-          fail: reject
-          complete: init.complete
-        }
-      RequestMQ.request(param)
-    })
 }
