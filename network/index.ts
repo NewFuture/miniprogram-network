63a5f41386b3cc3b5d30c918831f51e89638d922
New <github@newfuture.cc>
2018-12-05 00:47:37 +0800

    network

---
+++ network/index.ts
@@ -0,0 +1,45 @@
+import { Request, Http } from "miniprogram-request";
+import { Upload, Uploader } from "miniprogram-uploader";
+import { Download, Downloder } from "miniprogram-downloader";
+
+export {
+    Http,
+    Request,
+    RequestOption,
+    RequestConfig,
+    RequestInit,
+    RequestParams,
+    transformRequestResponseOkData,
+    transformRequestSendDefault,
+    CancelToken,
+} from "miniprogram-request";
+
+export {
+    Uploader,
+    Upload,
+    UploadOption,
+    UploadInit,
+    UploadParams,
+    transformUploadResponseOkData,
+    transformUploadSendDefault,
+} from 'miniprogram-uploader';
+
+export {
+    Downloder,
+    Download,
+    DownloadOption,
+    DownloadInit,
+    DownloadParams,
+    transformDownloadResponseOkData,
+    transfomDownloadSendDefault,
+} from 'miniprogram-downloader'
+
+// export const Netwrok: Readonly<{
+//     Http: Http,
+//     Uploader: Uploader,
+//     Downloder: Downloder,
+// }> = {
+//     Http: Request,
+//     Uploader: Upload,
+//     Downloder: Download,
+// }
