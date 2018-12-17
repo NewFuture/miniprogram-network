9231569734f971f28369a1e2261597acf0093c08
New <github@newfuture.cc>
2018-12-18 00:49:56 +0800

    remove dependency miniprogram-api-typings

--- network/test/methods.ts
+++ network/test/methods.ts
@@ -1,11 +1,21 @@
 
-import {Request, get, del, upload, download } from "../index";
+import { Network, get, del, upload, download, request } from "../index";
+import { Upload } from "../../uploader/dist";
 
-get('test').then((r)=>r);
+get('test').then((r) => r);
 
 get<string>('s')
 del('ss').then(console.log)
 
-upload('ss','testname');
+upload('ss', 'testname');
 
 download('ss');
+download({
+    url: 'sss',
+    jump: true,
+    onHeadersReceived: function (res) {
+        console.log(res.header)
+    }
+} as Network.DownloadOption).then(res=>res.statusCode)
+
+Upload.Defaults.transformResponse =f
