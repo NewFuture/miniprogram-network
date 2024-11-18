704c3dce5f4b0ec33a79ba682536086a35ae0ecf
New Future <NewFuture@users.noreply.github.com>
2024-11-18 21:18:36 +0800

    fix documents (#68)

--- request/test/config.ts
+++ request/test/config.ts
@@ -1,16 +1,16 @@
-import { Request, Http } from '../index';
+import { REQUEST, Http, transformRequestSendDefault, transformRequestResponseOkData } from '../src/index';
 
 //每次请求全部可配置参数
 //已设置的参数会覆盖默认参数
 //仅对此次请求生效
-Request.request({
+REQUEST.request({
     url: 'items',
     method: 'POST',
     data: {},
     /**
      * 取消操作的 CancelToken 
      */
-    cancelToken: null,
+    cancelToken: undefined,
     /**
      * 请求的根目录
      * Base URL for request
@@ -28,7 +28,7 @@ Request.request({
      * the path parameters to be replace in path
      * Must be a plain object
      */
-    params: null,
+    params: {},
 
     /**
     * 重试次数 
@@ -46,27 +46,27 @@ Request.request({
      * 异步返回promise
      * You may modify the data or headers object before it is sent.
      */
-    transformRequest: Http.RequestTransformation,
+    transformSend: transformRequestSendDefault,
     /**
      * 返回数据修改，返回值作为then的输入, throw exception 抛给catch
      * 异步返回Promise
      * allows changes to the response data to be made before it is passed to then/catch
      *  @example `res=>res.data`
      */
-    transformResponse: Http.ResponseTransformation
+    transformResponse: transformRequestResponseOkData
 })
 
 // 快速请求配置参数
-Request.post('items', {}, {
+REQUEST.post('items', {}, {
     //除了method,url和 data 不能设置其他均可设置
-    cancelToken: null,
+    cancelToken: undefined,
     baseURL: 'https://qq.com/',
     headers: {},
-    params: null,
+    params: undefined,
     retry: 3,
     responseType: "json",
-    transformRequest: Http.RequestTransformation,
-    transformResponse: Http.ResponseTransformation
+    transformSend: transformRequestSendDefault,
+    transformResponse: transformRequestResponseOkData,
 })
 
 
@@ -75,11 +75,13 @@ const http = new Http({
     //除了method,url，data 和cancelToken不能设置其他均可设置
     baseURL: 'https://qq.com/',
     headers: {},
-    params: null,
+    params: undefined,
     retry: 3,
     responseType: "json",
-    transformRequest: Http.RequestTransformation,
-    transformResponse: Http.ResponseTransformation
+    transformSend(options) {
+        return transformRequestSendDefault(options);
+    },
+    transformResponse: transformRequestResponseOkData
 })
 
 http.get('test');//将采用默认配置
